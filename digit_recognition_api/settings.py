ab7ccaf15739c70593d493b852ef080402153c77
ArmandThomas <arthomas2878@gmail.com>
2023-09-18 19:21:39 +0200

    add django cors header

--- digit_recognition_api/settings.py
+++ digit_recognition_api/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
   'django.contrib.messages',
   'django.contrib.staticfiles',
   'digit_recognition',
+  'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,8 @@ MIDDLEWARE = [
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
   'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'corsheaders.middleware.CorsMiddleware',
+  'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'digit_recognition_api.urls'
@@ -116,3 +119,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
