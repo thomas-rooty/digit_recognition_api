f23a931bbc3999756217f6e0fd3a4c42b39c1653
ArmandThomas <arthomas2878@gmail.com>
2023-09-19 09:37:43 +0200

    add pymongo

--- digit_recognition/views.py
+++ digit_recognition/views.py
@@ -6,46 +6,73 @@ from .model_loader import load_digit_recognition_model
 
 # Load your digit recognition model
 model = load_digit_recognition_model()
+import pymongo
+
+client = pymongo.MongoClient('mongodb+srv://projetUser:projetUser@clustercoursmern.fmzajui.mongodb.net/')
+dbname = client['digit-recognition']
+collection = dbname['stockage']
 
 
+# Create your views here.
 @csrf_exempt
 def recognize_digit(request):
-  if request.method == 'POST':
-    image_data = request.FILES.get('image')
+    if request.method == 'POST':
+        image_data = request.FILES.get('image')
 
-    if image_data is None:
-      return JsonResponse({'error': 'No image file provided'})
+        if image_data is None:
+            return JsonResponse({'error': 'No image file provided'})
 
-    try:
-      # Read the image data into a PIL Image
-      image = Image.open(image_data)
+        # Load your digit recognition model
+        model = load_digit_recognition_model()
 
-      # Convert to grayscale
-      image = ImageOps.grayscale(image)
+        try:
+            # Read the image data into a PIL Image
+            image = Image.open(image_data)
 
-      # Resize image
-      image = image.resize((28, 28))
+            # Convert to grayscale
+            image = ImageOps.grayscale(image)
 
-      # Convert to numpy array
-      image = np.array(image)
+            # Resize image
+            image = image.resize((28, 28))
 
-      # Invert image
-      image = 255 - image
+            # Convert to numpy array
+            image = np.array(image)
 
-      # Normalize image
-      image = image / 255
+            # 3. Convert 3D array to 2D list of lists
+            lst = []
+            for row in image:
+                tmp = []
+                for col in row:
+                    tmp.append(str(col))
+                lst.append(tmp)
 
-      # Reshape image
-      image = image.reshape(1, 28, 28, 1)
+            # Invert image
+            image = 255 - image
 
-      # Make predictions using the model
-      predicted_digit = model.predict(image)
+            # Normalize image
+            image = image / 255
 
-      # Convert the prediction to an integer
-      recognized_digit = int(np.argmax(predicted_digit))
+            # Reshape image
+            image = image.reshape(1, 28, 28, 1)
 
-      return JsonResponse({'digit': recognized_digit, 'confidence': float(predicted_digit[0][recognized_digit])})
-    except Exception as e:
-      return JsonResponse({'error': str(e)})
-  else:
-    return JsonResponse({'error': 'POST method required'})
+            # Make predictions using the model
+            predicted_digit = model.predict(image)
+
+            # Convert the prediction to an integer
+            recognized_digit = int(np.argmax(predicted_digit))
+
+            document = {
+                'digit': recognized_digit,
+                'confidence': float(predicted_digit[0][recognized_digit]),
+            }
+
+            # Add each pixel to the document
+            for i, row in enumerate(lst):
+              print(i)
+              print(row)
+
+            return JsonResponse({'digit': recognized_digit, 'confidence': float(predicted_digit[0][recognized_digit])})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+    else:
+        return JsonResponse({'error': 'POST method required'})
